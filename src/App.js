bb129e1d6639f4bbee7c0dca1b651b0161d7ca59
Chirag <chiragadve4@gmail.com>
2024-06-01 21:47:34 +0530

    State change

--- src/App.js
+++ src/App.js
@@ -8,15 +8,13 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
-  const [test, setTest] = useState({ name: "Jonas" });
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
   }
   function handleNext() {
     if (step < 3) setStep(step + 1);
-
-    setTest({ name: "Fred" });
   }
   return (
     <div className="steps">
